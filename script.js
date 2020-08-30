8448c7554d84d394e4168b625069b4d5225d3e67
Beatrice Del Toro <67802705+BeaDT@users.noreply.github.com>
2020-08-30 01:06:26 +0100

    Built API hourly forecast update

--- script.js
+++ script.js
@@ -47,10 +47,49 @@ function displayWeather(response) {
   celsiusTemperature = response.data.main.temp;
 }
 
+function formatHours(timestamp) {
+  let currentTime = new Date(timestamp);
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `<div class="col-2">
+              <h3>
+              ${formatHours(forecast.dt * 1000)}
+              </h3>
+              <img src="http://openweathermap.org/img/wn/${
+                forecast.weather[0].icon
+              }@2x.png"/>
+              <div class="weekday-temp">${Math.round(
+                forecast.main.temp_max
+              )}°|${Math.round(forecast.main.temp_min)}°</div>
+            </div>`;
+  }
+}
+
 function search(city) {
   let apiKey = "2c01c84d2f25936279dc3368bd18575f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function citySearchSubmit(event) {
